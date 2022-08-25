5c3e353665f306bdaa683c7aa19ebc8e5ee5f5cd
Sviatlana Karaliova <pavel.ivanov.06098885@gmail.com>
2022-08-25 10:51:24 +0200

    feat add script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const azathoth = document.querySelectorAll('azathoth'),
+      cthulthu = document.querySelectorAll('cthulthu'),
+      iogSothoth = document.querySelectorAll('iogSothoth'),
+      shubNiggurath = document.querySelectorAll('shubNiggurath');
